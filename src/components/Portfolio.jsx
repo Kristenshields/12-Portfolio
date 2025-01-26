2293732ad451efaea510d68501e4fcede27148c4
Kristen Shields <shieldskristen0@gmail.com>
2025-01-25 17:20:48 -0700

    added projects to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,6 +2,10 @@ import Project from "./Project";
 import '../styles/Portfolio.css';
 import projectOneImage from "../assets/projectOne.jpg";
 import projectTwoImage from "../assets/fantasyFighter.jpg";
+import projectThreeImage from "../assets/candidateSearchPage.jpg";
+import projectFourImage from "../assets/weatherDashboard.jpg";
+import comingSoonPhoto from "../assets/comingSoon.jpg";
+
 
 export default function Portfolio() {
   const projects = [
@@ -12,10 +16,34 @@ export default function Portfolio() {
       repoLink: "https://github.com/samvs-tech/Organizability",
     },
     {
-      title: "Project 2",
+      title: "Fantasy Fighter",
       image: projectTwoImage,
       deployedLink: "https://project-2-c43n.onrender.com/",
       repoLink: "https://github.com/MagicInUse/Fantasy-Fighter",
+    },
+    {
+      title: "Candidate Finder",
+      image: projectThreeImage,
+      deployedLink: "https://challenge-13-candidate-search-wbak.onrender.com/",
+      repoLink: "https://github.com/Kristenshields/Candidate-Finder",
+    },
+    {
+      title: "Weather Dashboard",
+      image: projectFourImage,
+      deployedLink: "https://challenge-9-weather-dashboard.onrender.com/",
+      repoLink: "https://github.com/Kristenshields/Weather-dashboard",
+    },
+    {
+      title: "Project 5",
+      image: comingSoonPhoto,
+      deployedLink: "",
+      repoLink: "https://github.com/Kristenshields",
+    },
+    {
+      title: "Project 6",
+      image: comingSoonPhoto,
+      deployedLink: "",
+      repoLink: "https://github.com/Kristenshields",
     },
     // Add more projects as needed
   ];
